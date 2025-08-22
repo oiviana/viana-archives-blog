47b10e988696ebc37f9bd7f4db08add580145504
oiviana <vianalucas80@gmail.com>
2025-08-22 17:02:47 -0300

    test: link for post page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import { BlogPostProps } from "@/types/post";
 import { getPosts } from "@/utils/getPosts";
+import Link from "next/link";
 
 export default async function Home() {
-   const posts: BlogPostProps[] = await getPosts()
+  const posts: BlogPostProps[] = await getPosts()
   console.log("posts ", posts)
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {posts?.map((post, index)=> (
-        <div key={index} className="flex bg-amber-300">
+      {posts?.map((post, index) => (
+        <Link
+          key={index}
+          href={`/artigos/${post.slug.current}`}
+          className="flex bg-amber-300 p-4 rounded hover:bg-amber-400 transition"
+        >
           {post.postTitle}
-        </div>
+        </Link>
       ))}
     </div>
   );
