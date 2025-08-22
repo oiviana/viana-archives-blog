8ec10edd333a07e064da8c81df3994ab54c8224c
oiviana <vianalucas80@gmail.com>
2025-08-22 17:01:31 -0300

    feat: add props fetch

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,5 +1,5 @@
 import { sanityClient } from '../lib/sanityClient'
 
 export async function getPosts() {
-  return sanityClient.fetch(`*[_type == "blogPost"]{postTitle, postDescription, slug, thumbnailImage, postCategory, authorName}`)
+  return sanityClient.fetch(`*[_type == "blogPost"]{postTitle, postDescription, slug, thumbnailImage, postCategory, authorName, readingTime}`)
 }
