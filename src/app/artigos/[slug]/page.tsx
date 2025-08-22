623da4b7ea6a1db0e341113aa52a0934c52fa144
oiviana <vianalucas80@gmail.com>
2025-08-22 17:03:01 -0300

    test: post page

---
+++ src/app/artigos/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { getPostBySlug } from "../../../utils/getPostBySlug";
+import { BlogPostContent } from "../../../components/PostContent";
+import { BlogPostProps } from "@/types/post";
+
+type PostPageProps = {
+  params: Promise<{ slug: string }>; // 👈 params é uma Promise
+};
+
+export default async function PostPage({ params }: PostPageProps) {
+    const { slug } = await params
+    const post:BlogPostProps = await getPostBySlug(slug);
+console.log("post ", post)
+    if (!post) {
+        return <div>Post não encontrado</div>;
+    }
+
+    return (
+        <article className="prose lg:prose-xl max-w-3xl mx-auto py-10">
+            <h1>{post.postTitle}</h1>
+            <BlogPostContent content={post.postContent} />
+        </article>
+    );
+}
