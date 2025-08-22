1d1553c699dfc658c5a0f09a8aff39a2a0df64a0
oiviana <vianalucas80@gmail.com>
2025-08-22 12:55:36 -0300

    test: initial posts fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { getPosts } from "@/utils/getPosts";
 
-export default function Home() {
+export default  async function Home() {
+    const posts = await getPosts()
+console.log("posts ",posts)
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
   <h2>Hello</h2>
--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'next-sanity'
 
-export const sanity = createClient({
+export const sanityClient = createClient({
   projectId: 'juuo6rlg', 
   dataset: 'production', 
   apiVersion: '2025-08-21', 
