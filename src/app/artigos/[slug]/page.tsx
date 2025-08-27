da29bcca61c7edddbf4204e6a1a801fcd220baef
oiviana <vianalucas80@gmail.com>
2025-08-27 16:17:11 -0300

    post page and grid

--- src/app/artigos/[slug]/page.tsx
+++ src/app/artigos/[slug]/page.tsx
@@ -1,22 +1,71 @@
 import { getPostBySlug } from "../../../utils/sanity/getPostBySlug";
 import { BlogPostContent } from "../../../components/PostContent";
 import { BlogPostProps } from "@/types/post";
+import Image from "next/image";
+import { generateImageUrl } from "@/utils/sanity/sanityImageBuilder";
+import Link from "next/link";
+import { IoArrowBack } from "react-icons/io5";
 
 type PostPageProps = {
-  params: Promise<{ slug: string }>; // 👈 params é uma Promise
+    params: Promise<{ slug: string }>;
 };
 
 export default async function PostPage({ params }: PostPageProps) {
     const { slug } = await params
-    const post:BlogPostProps = await getPostBySlug(slug);
+    const post: BlogPostProps = await getPostBySlug(slug);
 
     if (!post) {
         return <div>Post não encontrado</div>;
     }
 
     return (
-        <article className="prose lg:prose-xl max-w-3xl mx-auto py-10">
-            <h1>{post.postTitle}</h1>
+        <article className="prose lg:prose-xl max-w-[850px] mx-auto py-10">
+
+            <Link href="#" className="flex items-center gap-3 my-3">
+            <IoArrowBack/>
+            Voltar
+            </Link>
+        
+            <div className="relative rounded-2xl overflow-hidden h-[476px]">
+                <Image
+                    src={generateImageUrl(post.thumbnailImage)}
+                    width={850}
+                    height={180}
+                    alt={post.postTitle}
+                    objectFit="contain"
+                    className="border-4 border-[#141217] rounded-2xl"
+                    priority={true}
+                    loading="eager"
+                />
+                <div className="absolute bg-gradient-to-t from-[#141217] from-35% to-transparent w-full bottom-0 p-2 px-4 rounded-b-2xl" >
+                    <h1 className="text-lg lg:text-3xl mb-3">{post.postTitle}</h1>
+                    <div className="flex items-center gap-4">
+                        <Image
+                            src={generateImageUrl(post.authorImage)}
+                            width={56}
+                            height={56}
+                            alt={post.postTitle}
+                            objectFit="contain"
+                            className="rounded-full"
+                        />
+                        <div className="text-lg">
+                            <span>Autor: </span> <span>{post.authorName}</span>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col my-7 gap-3">
+                <span>Tempo de leitura: {post.readingTime} minutos.</span>
+                <span>
+                    Publicado em:{" "}
+                    {new Date(post.postDate).toLocaleDateString("pt-BR", {
+                        day: "2-digit",
+                        month: "long",
+                        year: "numeric",
+                    })}{"."}
+                </span>
+            </div>
             <BlogPostContent content={post.postContent} />
         </article>
     );
