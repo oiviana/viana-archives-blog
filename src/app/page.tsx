4bfa82742ed607223f47c2b61ae941b2d5b7e9a9
oiviana <vianalucas80@gmail.com>
2025-08-27 16:14:58 -0300

    feat: skeleton loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react"
 import PostsGrid from "../components/PostGrid"
+import Skeleton from "@/components/Skeleton"
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20">
-      <Suspense fallback={<h2 className="text-xl font-semibold">Carregando...</h2>}>
+      <Suspense fallback={<Skeleton skeletonType="grid-cards" />}>
         <PostsGrid />
       </Suspense>
     </div>
