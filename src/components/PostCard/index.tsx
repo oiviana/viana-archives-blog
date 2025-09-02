17002d37cee36982ae2dee1f5cebb416703ba2db
oiviana <vianalucas80@gmail.com>
2025-09-02 11:38:08 -0300

    feat: add flag

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -14,23 +14,23 @@ interface PostCardProps {
 
 export default function PostCard({ postTitle, slug, thumbnail, postDescription, postCategory, readingTime }: PostCardProps) {
     return (
-        <Link className="bg-[#23212c] rounded w-full max-w-[320px] h-[380px] p-2 flex flex-col justify-start post-card shadow"
+        <Link className="bg-[#23212c] rounded w-full max-w-[320px] h-[380px] p-2 flex flex-col justify-start post-card shadow relative"
             href={`/artigos/${slug}`}
         >
-            <div className="overflow-hidden max-h-[180px]">
+            <div className="overflow-hidden h-[180px]">
                 <Image
                     src={thumbnail}
                     width={305}
                     height={180}
                     alt={postTitle}
                     loading="eager"
-                    className="object-cover"
+                    className="object-cover h-full"
                 />
             </div>
             <div className="flex flex-col">
                 <CategoryFlag category={postCategory}/>
-                <span className="text-white-secondary font-lora text-sm my-2">Tempo de leitura: {readingTime} minutos</span>
-                <h2 className="text-lg lg:text-xl my-2.5 font-raleway text-green-main">{postTitle}</h2>
+                <span className="text-white-secondary font-lora text-sm my-2 mt-4">Tempo de leitura: {readingTime} minutos</span>
+                <h2 className="text-lg lg:text-xl my-2 font-jetbrains text-white-main font-semibold">{postTitle}</h2>
                 <h3 className="font-lora">{postDescription}</h3>
             </div>
         </Link>
