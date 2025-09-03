c7b30b238d02db7731d6117971c313d5a5770deb
Lucas Viana <vianalucas80@gmail.com>
2025-09-02 22:44:03 -0300

    feat: header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
+import Image from "next/image"
 
 export default function Header() {
 
     return (
-        <header className="bg-background-bold/80 fixed z-50 w-full h-16 lg:h-[70px] shadow-lg  backdrop-blur-lg">
+        <header className="bg-background-bold/80 fixed z-50 w-full h-16 lg:h-[80px] shadow-lg  backdrop-blur-lg">
             <div className="w-full h-full max-w-5xl mx-auto flex justify-between items-center px-3.5 lg:px-0">
-                <span>Logo</span>
+                <Link
+                    href={"/"}>
+                    <Image
+                        src={"/logo.png"}
+                        width={90}
+                        height={50}
+                        className="w-[65px] h-auto lg:w-[90px]"
+                        alt="Logo do site"
+                    />
+                </Link>
                 <nav>
                     <ul className="flex gap-8 lg:gap-12">
                         <li>
@@ -18,6 +28,7 @@ export default function Header() {
                         </li>
                         <li className="flex lg:hidden">
                             <Link href={"https://www.linkedin.com/in/lucasviana80/"}
+
                                 className="font-jetbrains hover:text-green-main transition-colors"
                             >
                                 <FaLinkedin
