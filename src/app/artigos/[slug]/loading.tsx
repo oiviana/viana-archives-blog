31f2bd4c97ed43c63e00ac506baf22341e8b6c00
oiviana <vianalucas80@gmail.com>
2025-09-04 16:56:41 -0300

    feat: meta on loading page

--- src/app/artigos/[slug]/loading.tsx
+++ src/app/artigos/[slug]/loading.tsx
@@ -1,20 +1,5 @@
 import Header from "@/components/Header";
 import Skeleton from "@/components/Skeleton";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Carregando",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "any" },
-    ],
-    apple: "/apple-touch-icon.png",
-    shortcut: "/favicon.ico",
-  },
-  manifest: "/site.webmanifest",
-};
 
 export default function Loading() {
     return (
