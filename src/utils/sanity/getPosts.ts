81dbb447b43e692146fafc5261e4a9ebae1e2b77
Lucas Viana <vianalucas80@gmail.com>
2025-09-05 11:37:41 -0300

    test: revalidate fetch

--- src/utils/sanity/getPosts.ts
+++ src/utils/sanity/getPosts.ts
@@ -1,5 +1,17 @@
 import { sanityClient } from '@/lib/sanityClient'
 
 export async function getPosts() {
-  return sanityClient.fetch(`*[_type == "blogPost"]{postTitle, postDescription, slug, thumbnailImage, postCategory, authorName, readingTime}`)
+  return sanityClient.fetch(
+    `*[_type == "blogPost"]{
+      postTitle,
+      postDescription,
+      slug,
+      thumbnailImage,
+      postCategory,
+      authorName,
+      readingTime
+    }`,
+    {}, 
+    { next: { revalidate: 60, } } 
+  )
 }
