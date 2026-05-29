4ccb0e49fb846d666741f3e23debc45a13bb6dc4
oiviana <vianalucas80@gmail.com>
2026-05-29 16:28:15 -0300

    vtex flag

--- src/components/CategoryFlag/index.tsx
+++ src/components/CategoryFlag/index.tsx
@@ -9,23 +9,27 @@ interface FlagProps extends CategoryFlagProps {
 
 function Flag({ category, foreground, background }: FlagProps) {
   return (
-    <span className={`category-flag flex justify-center items-center min-w-20 text-sm lg:text-base font-jetbrains py-1 px-2 lg:px-3 rounded-xl  border-1 text-[${foreground}] border-[${foreground}] ${background}`}
-      style={{ color: foreground, borderColor: foreground, background: background }}>
+    <span
+      className="category-flag flex justify-center items-center min-w-20 text-sm lg:text-base font-jetbrains py-1 px-2 lg:px-3 rounded-xl border"
+      style={{ color: foreground, borderColor: foreground, background }}
+    >
       {category}
     </span>
   )
 }
 
 export default function CategoryFlag({ category }: CategoryFlagProps) {
-  switch (category) {
-    case "Codes":
-      return <Flag category={category} foreground={"#d8e7f3"} background={"#2F6B9A"} />
-    case "React Native":
-      return <Flag category={category} foreground={"#d8e7f3"} background={"#2F6B9A"} />
-    case "IA":
-      return <Flag category={category} foreground={"#0d0d0d"} background={"#b3b3b3"} />
-    case "Carreira":
-      return <Flag category={category} foreground={"#fef5e6"} background={"#976435"} />
+  switch (category.toLowerCase()) {
+    case "vtex":
+      return <Flag category={category} foreground="#fee7f0" background="#F30E6D" />
+    case "codes":
+      return <Flag category={category} foreground="#d8e7f3" background="#2F6B9A" />
+    case "react native":
+      return <Flag category={category} foreground="#d8e7f3" background="#2F6B9A" />
+    case "ia":
+      return <Flag category={category} foreground="#0d0d0d" background="#b3b3b3" />
+    case "carreira":
+      return <Flag category={category} foreground="#fef5e6" background="#976435" />
     default:
       return null
   }
