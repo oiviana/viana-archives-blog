d45e191b4b06c72e3b855e08f951eead825da882
oiviana <vianalucas80@gmail.com>
2026-06-13 16:28:14 -0300

    footer pb

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="mx-auto mt-16 w-full max-w-5xl border-t border-background-extralight px-4 py-8 text-center font-jetbrains text-sm text-white-secondary lg:mt-20">
+    <footer className="mx-auto mt-16 w-full max-w-5xl border-t border-background-extralight px-4 py-8 pb-4 text-center font-jetbrains text-sm text-white-secondary lg:mt-20">
       <span>
         {currentYear} © Viana Archives
       </span>
